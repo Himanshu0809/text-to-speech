b4d0b766137ceb8ebf6f08d02cf60b8561cbcb37
Himanshu <guptahimanshu479@gmail.com>
2020-01-13 08:13:41 +0530

    text-to-speech complete

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const say = require("say"),
     prompt = require("prompt");
+
+prompt.start();
+
+startSpeak();
+
+//function to make it continuous
+function startSpeak() {
+    //prompt to take the message
+    prompt.get(['message'], (error, result) => {
+        //say to turn it into speech
+        say.speak(result.message, '', 2.0, () => {
+            //if the message is Goodbye the function will exit
+            if (result.message == 'Goodbye') {
+                process.exit();
+            }
+            startSpeak();
+        });
+    });
+}
